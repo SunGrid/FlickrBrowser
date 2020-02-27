39ce4d31f1d1c53886977397ec058b87aa6afa97
Michael <mlonnecker@gmail.com>
2020-02-27 12:04:50 -0700

    finished serializable part in PhotoDetailsActivity with Picasso

--- app/src/main/java/com/vivospice/flickrbrowser/PhotoDetailsActivity.kt
+++ app/src/main/java/com/vivospice/flickrbrowser/PhotoDetailsActivity.kt
@@ -1,17 +1,28 @@
 package com.vivospice.flickrbrowser
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-
-import kotlinx.android.synthetic.main.activity_photo_details.*
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.content_photo_details.*
 
 class PhotoDetailsActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_details)
+
         activateToolbar(true)
+
+        val photo = intent.getSerializableExtra(PHOTO_TRANSFER) as Photo
+
+        photo_title.text = photo.title
+        photo_tags.text = photo.tags
+        photo_author.text = photo.author
+
+        Picasso.get()
+            .load(photo.link)
+            .error(R.drawable.placeholder_48dp)
+            .placeholder(R.drawable.placeholder_48dp)
+            .into(photo_image)
     }
 
 }
