126a8b6a07977268ceee3b760e445ade0b4c9aba
Michael <mlonnecker@gmail.com>
2020-02-28 19:47:28 -0700

    layoutParams change when image is clicked and immersiveModeEnable thingy in PhotoDetailsActivity

--- app/src/main/java/com/vivospice/flickrbrowser/PhotoDetailsActivity.kt
+++ app/src/main/java/com/vivospice/flickrbrowser/PhotoDetailsActivity.kt
@@ -1,8 +1,15 @@
 package com.vivospice.flickrbrowser
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.content_photo_details.*
+
 
 class PhotoDetailsActivity : BaseActivity() {
 
@@ -24,6 +31,86 @@ class PhotoDetailsActivity : BaseActivity() {
             .error(R.drawable.placeholder_48dp)
             .placeholder(R.drawable.placeholder_48dp)
             .into(photo_image)
+
+
+        var isImageFitToScreen: Boolean = false
+        photo_image.setOnClickListener{
+
+            if(isImageFitToScreen) {
+                fullScreen()
+                photo_image.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+                photo_image.scaleType = ImageView.ScaleType.FIT_CENTER
+                photo_image.adjustViewBounds = true
+                photo_image.maxHeight = 500
+                Toast.makeText(this,"FIT_CENTER, maxHeight = 500", Toast.LENGTH_SHORT).show()
+                isImageFitToScreen = false
+            } else {
+                fullScreen()
+                photo_image.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+                photo_image.scaleType = ImageView.ScaleType.FIT_XY
+                photo_image.adjustViewBounds = false
+                Toast.makeText(this,"FIT_XY and FullScreen", Toast.LENGTH_SHORT).show()
+                isImageFitToScreen = true
+            }
+        }
+    }
+
+//https://developer.android.com/training/system-ui/immersive.html
+    private fun fullScreen(){
+// BEGIN_INCLUDE (get_current_ui_flags)
+        // The UI options currently enabled are represented by a bitfield.
+        // getSystemUiVisibility() gives us that bitfield.
+        // BEGIN_INCLUDE (get_current_ui_flags)
+// The UI options currently enabled are represented by a bitfield.
+// getSystemUiVisibility() gives us that bitfield.
+        val uiOptions = window.decorView.systemUiVisibility
+        var newUiOptions = uiOptions
+        // END_INCLUDE (get_current_ui_flags)
+        // BEGIN_INCLUDE (toggle_ui_flags)
+        // END_INCLUDE (get_current_ui_flags)
+// BEGIN_INCLUDE (toggle_ui_flags)
+        val isImmersiveModeEnabled =
+            uiOptions or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY == uiOptions
+        if (isImmersiveModeEnabled) {
+            Log.d("fullScreen", "Turning immersive mode mode off. ")
+        } else {
+            Log.d("fullScreen", "Turning immersive mode mode on.")
+        }
+
+        // Navigation bar hiding:  Backwards compatible to ICS.
+        // Navigation bar hiding:  Backwards compatible to ICS.
+        if (Build.VERSION.SDK_INT >= 14) {
+            newUiOptions = newUiOptions xor View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        }
+
+        // Status bar hiding: Backwards compatible to Jellybean
+        // Status bar hiding: Backwards compatible to Jellybean
+        if (Build.VERSION.SDK_INT >= 16) {
+            newUiOptions = newUiOptions xor View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
+
+        // Immersive mode: Backward compatible to KitKat.
+        // Note that this flag doesn't do anything by itself, it only augments the behavior
+        // of HIDE_NAVIGATION and FLAG_FULLSCREEN.  For the purposes of this sample
+        // all three flags are being toggled together.
+        // Note that there are two immersive mode UI flags, one of which is referred to as "sticky".
+        // Sticky immersive mode differs in that it makes the navigation and status bars
+        // semi-transparent, and the UI flag does not get cleared when the user interacts with
+        // the screen.
+        // Immersive mode: Backward compatible to KitKat.
+// Note that this flag doesn't do anything by itself, it only augments the behavior
+// of HIDE_NAVIGATION and FLAG_FULLSCREEN.  For the purposes of this sample
+// all three flags are being toggled together.
+// Note that there are two immersive mode UI flags, one of which is referred to as "sticky".
+// Sticky immersive mode differs in that it makes the navigation and status bars
+// semi-transparent, and the UI flag does not get cleared when the user interacts with
+// the screen.
+        if (Build.VERSION.SDK_INT >= 18) {
+            newUiOptions = newUiOptions xor View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        }
+
+        window.decorView.systemUiVisibility = newUiOptions
+        //END_INCLUDE (set_ui_flags)
     }
 
 }
